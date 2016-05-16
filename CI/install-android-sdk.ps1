8f5f1a043d50f3b66fdd7dfb6a26e6ceedbe020d
Caleb Clarke <TheAlmightyBob@users.noreply.github.com>
2016-05-15 23:26:57 -0700

    Trying to update CI for android license change

--- CI/install-android-sdk.ps1
+++ CI/install-android-sdk.ps1
@@ -24,7 +24,7 @@ Function Install-AndroidSDK() {
 
     $sdkIndexes = $sdks |% { $_.Index } 
     $sdkIndexArgument = [string]::Join(',', $sdkIndexes) 
-    Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument 
+    Echo 'yy' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument 
 }
 
 $sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 15*' -or $_.name -like 'google apis*api 15' } 
